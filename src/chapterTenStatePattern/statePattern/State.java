c6e57d7c4a8aaccc217238eb5a699885e22cfb1f
kshkumar <kshkumar@adobe.com>
2020-06-04 15:24:17 +0530

    Revert "Adding the state pattern as well as the simple implementation without state pattern"
    This reverts commit 982a67d5

--- src/chapterTenStatePattern/statePattern/State.java
+++
@@ -1,8 +0,0 @@
-package chapterTenStatePattern.statePattern;
-
-public interface State {
-  public void insertQuarter();
-  public void ejectQuarter();
-  public void turnCrank();
-  public void dispense();
-}
